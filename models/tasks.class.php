7aa32a395682acf77f7a93706ce205bab905d57d
Kempis Guerrero <36332963+KempisGV@users.noreply.github.com>
2020-08-04 20:02:43 -0500

    Update tasks.class.php
    Se eliminaron las propiedades de la clase Tasks, se modificaron las variables del método getTasks() y se agregaron los métodos getUID() y setTask()

--- models/tasks.class.php
+++ models/tasks.class.php
@@ -4,25 +4,22 @@ include './database/db.php';
 
 class Tasks extends Db{
     
-    private $records;
-    private $results;
-    private $json_array = array();
     /*Función que retorna JSON con las tareas del usuario */
     protected function getTasks(){
 
-
         if (!empty($_POST['email']) && !empty($_POST['password'])){
-            /*$this->records = $this->connect()->prepare('SELECT id, email, password FROM usuario WHERE email=:email');*/
-            $this->records = $this->connect()->prepare('SELECT tarea.title, tarea.description FROM tarea JOIN usuario ON usuario.id=tarea.uid WHERE usuario.email=:email');
-            $this->records->bindParam(':email', $_POST['email']);
-            $this->records->execute(); 
-            $this->results = $this->records->fetchAll();
+            $records = $this->connect()->prepare('SELECT tarea.title, tarea.description FROM tarea JOIN usuario ON usuario.id=tarea.uid WHERE usuario.email=:email');
+            $records->bindParam(':email', $_POST['email']);
+            $records->execute(); 
+            $results =  $records->fetchAll();
+
+            $json_array = array();
 
             /*Si $results no está vacío se crea un array con formato json con los valores que guarda $temp*/
-            if(!empty($this->results)){
-                
-                foreach($this->results as $row){
-                    $this->json_array[] = $row;
+            if(!empty($results)){
+
+                foreach($results as $row){
+                    $json_array[] = $row;
                 }
 
             } else{
@@ -30,9 +27,34 @@ class Tasks extends Db{
             }
         }
 
-        return  $this->json_array;
+        return  $json_array;
+    }
+
+    protected function getUID(){
+
+        if (!empty($_POST['email']) && !empty($_POST['password'])){
+            $query = 'SELECT id FROM usuario WHERE email=:email';
+            $records = $this->connect()->prepare($query);
+            $records->bindParam(':email', $_POST['email']);
+            $records->execute(); 
+            $results = $records->fetchAll();
+
+        } else{
+            return false;
+        }
+
+        return  $results[0]['id'];
+
+    }
+
+    /*Función que hace INSERT de nueva tarea*/
+    protected function setTask($uid, $title, $description){
+
+        $query = "INSERT INTO tarea(uid, title, description) VALUES(?, ?, ?)";
+        $records = $this->connect()->prepare($query);
+        $records->execute([$uid, $title, $description]);
+
     }
 
 }
-
 ?>
