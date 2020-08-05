625ffd13aeb357893ae01825a3b446e0b58c0851
Kempis Guerrero <36332963+KempisGV@users.noreply.github.com>
2020-08-04 19:57:33 -0500

    Update taskscontroller.class.php
    Se agregó el método createTask()

--- controllers/taskscontroller.class.php
+++ controllers/taskscontroller.class.php
@@ -1,6 +1,12 @@
 <?php
 
 class TasksController extends Tasks{
+
+    public function createTask($title, $description){
+                
+        $this->setTask($this->getUID(), $title, $description);
+
+    }
     
 }
 
