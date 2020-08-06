f8a9f31095dac52a55aa7f2a97b9560f2265d9ec
Kempis Guerrero <36332963+KempisGV@users.noreply.github.com>
2020-08-05 21:37:27 -0500

    Add files via upload

---
+++ controllers/userscontroller.class.php
@@ -0,0 +1,42 @@
+<?php
+    include_once '../models/usuarios.class.php';
+
+    class UsersController extends Usuario{
+
+        //Método que hace uso del método heredado saveUsuario() para guardar la información en la base de datos
+        public function guardarUsuario(){
+            $this->saveUsuario();
+        }
+
+        //Método que obtiene un array con todos los usuarios del método heredado getUsuarios(), al cual se le hace echo y se lo transforma a formato JSON
+        public static function obtenerUsuarios(){
+
+            $json = self::getUsuarios();
+            echo json_encode($json);
+        }
+
+        //Método que obtiene un array con el usuario al que corresponde el ID del método heredado getUsuario(), al cual se le hace echo y se lo transforma a formato JSON
+        public static function obtenerUsuario($indice){
+
+            $json = self::getUsuario($indice);
+            echo json_encode($json);
+
+        }
+
+        //Método que hace uso del método heredado updateUsuario() para actualizar la información de un usuario en la base de datos
+        public function actualizarUsuario($email, $password, $indice){
+
+            $this->updateUsuario($email, $password, $indice);
+
+        }
+
+        //Método que hace uso del método heredado deleteUsuario() para eliminar el usuario al que le corresponda el ID
+        public static function eliminarUsuario($indice){
+
+            self::deleteUsuario($indice);
+
+        }
+
+    }
+
+?>
